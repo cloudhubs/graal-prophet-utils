6759b969a807c6b0819ff69bf83a67492f0fe547
RichardHutcheson <richardhutch80@gmail.com>
2023-03-21 16:49:29 -0500

    create link and request objects

--- src/main/java/baylor/cloudhubs/prophetutils/visualizer/Link.java
+++ src/main/java/baylor/cloudhubs/prophetutils/visualizer/Link.java
@@ -1,5 +1,43 @@
 package baylor.cloudhubs.prophetutils.visualizer;
 
+import java.util.ArrayList;
+
 public class Link {
     
+    private String source;
+    private String target;
+    private ArrayList<Request> requests;
+
+    public Link(String source, String target, ArrayList<Request> requests){
+        this.source = source;
+        this.target = target;
+        this.requests = requests;
+    }
+    // Getter and setter for source variable
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    // Getter and setter for target variable
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    // Getter and setter for requests variable
+    public ArrayList<Request> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(ArrayList<Request> requests) {
+        this.requests = requests;
+    }
+
 }
--- src/main/java/baylor/cloudhubs/prophetutils/visualizer/Request.java
+++ src/main/java/baylor/cloudhubs/prophetutils/visualizer/Request.java
@@ -1,5 +1,57 @@
 package baylor.cloudhubs.prophetutils.visualizer;
 
 public class Request {
+    private String httpType;
+    private String uri;
+    private String requestReturn;
+    private String endpointFunction;
+    private String targetEndpoint;
     
+    public Request(String httpType, String uri, String requestReturn, String endpointFunction, String targetEndpoint) {
+        this.httpType = httpType;
+        this.uri = uri;
+        this.requestReturn = requestReturn;
+        this.endpointFunction = endpointFunction;
+        this.targetEndpoint = targetEndpoint;
+    }
+    
+    public String getHttpType() {
+        return httpType;
+    }
+
+    public void setHttpType(String httpType) {
+        this.httpType = httpType;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getRequestReturn() {
+        return requestReturn;
+    }
+
+    public void setRequestReturn(String requestReturn) {
+        this.requestReturn = requestReturn;
+    }
+
+    public String getEndpointFunction() {
+        return endpointFunction;
+    }
+
+    public void setEndpointFunction(String endpointFunction) {
+        this.endpointFunction = endpointFunction;
+    }
+
+    public String getTargetEndpoint() {
+        return targetEndpoint;
+    }
+
+    public void setTargetEndpoint(String targetEndpoint) {
+        this.targetEndpoint = targetEndpoint;
+    }
 }
